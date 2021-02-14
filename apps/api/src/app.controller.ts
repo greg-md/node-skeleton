84cf04afed5c687d391bc427493824331164f2de
Grigorii Duca <contact@greg.md>
2021-02-14 15:10:56 +0200

    Increase timeout.

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -14,7 +14,7 @@ export class AppController {
   async getHello(): Promise<string> {
     const result = await this.client
       .send<number>({ cmd: 'hello' }, {})
-      .pipe(timeout(5000))
+      .pipe(timeout(60000))
       .toPromise();
 
     await this.client.emit<number>('hello_sent', result).toPromise();
