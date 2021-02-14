c2057469827b88d145cf7f35f47633450d37d055
Grigorii Duca <contact@greg.md>
2021-02-14 23:13:13 +0200

    Remove not needed envs.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -23,13 +23,4 @@ file_env() {
 	unset "$fileVar"
 }
 
-file_env 'PRISMA_ENDPOINT'
-file_env 'PRISMA_GAMEPLAY_ENDPOINT'
-file_env 'PRISMA_MANAGEMENT_API_SECRET'
-file_env 'JWT_SECRET'
-file_env 'WS_REDIS_HOST'
-file_env 'WS_REDIS_PORT'
-file_env 'LOCK_REDIS_HOST'
-file_env 'LOCK_REDIS_PORT'
-
 exec "$@"
