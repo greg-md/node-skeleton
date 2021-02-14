c2a33c70f507a7bbe06480d87d24a31e385a661c
Grigorii Duca <contact@greg.md>
2021-02-14 20:22:11 +0200

    ls.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,22 +2,6 @@
 
 ls -all
 
-mkdir -p /node /node/dist /node/coverage
-
-ls -all
-
-chmod -R 777 /node/dist /node/coverage
-
-ls -all
-
-echo "USER GROUP"
-echo $USER
-echo $GROUP
-
-chown -R 1001:116 /node/dist /node/coverage
-
-ls -all
-
 npm run test:cov
 
 ./scripts/wait-for-it.sh api:3000 -s -t 0 -- \
