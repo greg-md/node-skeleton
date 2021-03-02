1d37bbbe7d6737703bb1b27b2de423c13400df01
Grigorii Duca <contact@greg.md>
2021-03-02 19:35:35 +0200

    Fix tests.

--- build-deploy/ci.sh
+++ build-deploy/ci.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$NODE_ENV" == "ci" ]; then
-    ./scripts/wait-for-it.sh api:3000 -s -t 0 -- \
+    ./build-deploy/wait-for-it.sh api:3000 -s -t 0 -- \
     npm run test:ci
 else
-    ./scripts/wait-for-it.sh api:3000 -s -t 0 -- \
+    ./build-deploy/wait-for-it.sh api:3000 -s -t 0 -- \
     npm run test:dev
 fi
