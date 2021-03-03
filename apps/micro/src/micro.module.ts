cac238c5cff92fda106aa542423fcbcdf4db383e
Grigorii Duca <contact@greg.md>
2021-03-03 20:16:25 +0200

    Apply DDD to micro.

--- apps/micro/src/micro.module.ts
+++ apps/micro/src/micro.module.ts
@@ -1,7 +1,7 @@
 import { CoreModule } from '@app/core';
 import { Module } from '@nestjs/common';
-import { MicroController } from './micro.controller';
-import { MicroService } from './micro.service';
+import { MicroController } from './application/micro.controller';
+import { MicroService } from './application/micro.service';
 
 @Module({
   imports: [CoreModule],
