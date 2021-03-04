e2beeaa93e6be6d838e3890d21232af6b9af26f8
Grigorii Duca <contact@greg.md>
2021-03-04 10:02:45 +0200

    Add ci back to separate container.

--- build-deploy/ci.sh
+++ build-deploy/ci.sh
@@ -2,8 +2,8 @@
 
 if [ "$NODE_ENV" == "ci" ]; then
     ./build-deploy/wait-for-it.sh nats:4222 -s -t 0 -- \
-    npx concurrently --raw "npm:start:dev -- micro"
+    npx concurrently --raw "npm:test:cov" "npm:test:e2e"
 else
     ./build-deploy/wait-for-it.sh nats:4222 -s -t 0 -- \
-    npx concurrently --raw "npm:start:dev -- api" "npm:start:dev -- micro"
+    npx concurrently --raw "npm:test:watch" "npm:test:e2e -- --watch"
 fi
